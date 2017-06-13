c7c21b28f765a0861523cee0428ee211b44a5d90
Andrew Plotkin <zarf@Zarfslab.local>
2017-06-13 00:32:04 -0400

    Put in the socket server. This is not yet connected to the Signal client.

--- main.js
+++ main.js
@@ -1,6 +1,68 @@
+const net = require('net');
+const child_process = require('child_process');
 const SignalClient = require('signal-client');
 
+var localport = 8000;
+if (process.argv.length >= 3)
+    localport = 1 * process.argv[2];
+
 const client = new SignalClient('zarf-signal-cli');
+var connections = [];
+
+function accept_connection(conn) {
+    console.log('client connected');
+    connections.push(conn);
+
+    var inbuf = Buffer.alloc(0);
+
+    conn.on('end', () => {
+        console.log('client disconnected');
+        connections = connections.filter(val => (val !== conn));
+    });
+
+    conn.on('data', (buf) => {
+        inbuf = Buffer.concat([ inbuf, buf ]);
+
+        while (inbuf.length) {
+            var pos = inbuf.indexOf(10);
+            if (pos < 0)
+                break;
+            var str = inbuf.toString('utf8', 0, pos+1);
+            str = str.trim();
+            if (str.length)
+                sendmessage(str);
+            inbuf = inbuf.slice(pos+1);
+        }
+    });
+}
+
+function sendmessage(str) {
+    setTimeout(gotmessage, 1000, str, '+12345');
+}
+
+function gotmessage(str, sender) {
+    if (!connections.length) {
+        var proc = child_process.execFile('terminal-notifier', [ '-title', 'Signal', '-subtitle', sender ]);
+        proc.stdin.write(str+'\n');
+        proc.stdin.end();
+    }
+    else {
+        for (var conn of connections) {
+            conn.write(str+'\n');
+        }
+    }
+}
+
+var server = net.createServer(accept_connection);
+
+server.on('error', (err) => {
+    throw err;
+});
+
+server.listen({ host:'localhost', port:localport }, () => {
+    console.log('server listening on port ' + localport + '...');
+});
+
 
 client.on('message', (ev) => {
     console.log('received message from', ev.data.source, ev.data);
