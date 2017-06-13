08925590703b0741d45077792abef20adc0242dc
Andrew Plotkin <zarf@Zarfslab.local>
2017-06-12 23:42:06 -0400

    Spacing!

--- main.js
+++ main.js
@@ -10,28 +10,28 @@ client.on('sent', (ev) => {
     console.log('sent a message to', ev.data.destination, ev.data);
 });
  
-client.on('receipt', (ev)=>{
+client.on('receipt', (ev) => {
     var message = ev.proto;
     // message.timestamp, message.source, message.sourceDevice
 });
  
  
-client.on('contact', (ev)=>{
+client.on('contact', (ev) => {
     // console.log('contact received', ev.contactDetails);
 });
  
-client.on('group', (ev)=>{
+client.on('group', (ev) => {
     // console.log('group received', ev.groupDetails);
 });
  
-client.on('read', (ev)=>{
+client.on('read', (ev) => {
     var read_at   = ev.timestamp;
     var timestamp = ev.read.timestamp;
     var sender    = ev.read.sender;
     // console.log('read receipt', sender, timestamp);
 });
  
-client.on('error', (ev)=>{
+client.on('error', (ev) => {
     console.log('error', ev.error, ev.error.stack);
 });
 
